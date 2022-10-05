96123973adb1c4bebfb33d41e0f1f3521f37c6b5
famurga <fmurga@unsa.edu.pe>
2022-10-05 16:40:31 -0500

    primer commit

---
+++ src/modules/daybook/store/journal/getters.js
@@ -0,0 +1,9 @@
+/* export const myGetter = (state) =>{
+    return state.algo
+} */
+export const getEntriesByTerm = (/* state */) =>{
+    
+}
+export const getEntriesById = (/* state */) =>{
+    
+}
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,28 @@
+export default () => ({
+
+    isLoading: true,
+    entries: [
+        {
+            id:new Date().getTime(),
+            date: new Date().toDateString(),
+            text: ' Magna ipsum qui aliquip ut tempor pariatur cillum fugiat proident consectetur esse est sunt. Est amet ex magna fugiat consequat dolore aliqua do ullamco. Excepteur ut excepteur adipisicing ipsum ex tempor enim ad in. Est commodo sunt id excepteur. Velit ex proident ut exercitation commodo officia qui cupidatat consequat',
+            picture:null
+
+        },
+        {
+            id:new Date().getTime()+1000,
+            date: new Date().toDateString(),
+            text: 'Pariatur officia occaecat reprehenderit irure nulla nulla pariatur laborum. Sunt qui do est est consectetur sit in sunt. Fugiat commodo velit sit nulla culpa cupidatat esse non velit nulla exercitation non proident do.',
+            picture:null
+
+        },
+        {
+            id:new Date().getTime()+1000,
+            date: new Date().toDateString(),
+            text: 'Nulla ad consequat nisi non enim dolore ad Lorem culpa reprehenderit excepteur id. Non ut duis occaecat adipisicing laborum aliquip elit consequat proident deserunt. Culpa amet nulla eu sunt esse exercitation incididunt Lorem. Aliqua deserunt est voluptate et fugiat cupidatat cillum magna.',
+            picture:null
+
+        },
+    ]
+
+})
